5f9c9dec1d7a252e05a24672fcff8849a894db72
navanth-reddy <gnavanthkumarreddy7@gmail.com>
2025-03-11 11:31:15 +0530

    First Commit

---
+++ src/com/Main.java
@@ -0,0 +1,15 @@
+package com;
+
+import com.model.Book;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Book book = new Book();
+		book.setTitle("Complete Java");
+		book.setAuthor("Herbert Schildt");
+		System.out.println(book);
+	}
+
+}
